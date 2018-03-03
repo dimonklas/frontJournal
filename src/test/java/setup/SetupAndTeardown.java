4264ef8da29ca331236e303f41d4e63472c0281c
Луговской Евгений Владимирович <evgenij.lugovskoj@privatbank.ua>
2018-03-03 10:51:19 +0200

    add environment ifo in report

--- src/test/java/setup/SetupAndTeardown.java
+++ src/test/java/setup/SetupAndTeardown.java
@@ -1,15 +1,34 @@
 package setup;
 
 import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import pages.LoginPage;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class SetupAndTeardown {
     LoginPage loginPage;
     ConfVars confVars = ConfVars.getInstance();
+    String browserName;
+    String browserVersion;
+    String platform;
+    String driverVersion;
 
     @BeforeSuite
     protected void setUp() {
@@ -21,7 +40,39 @@ public class SetupAndTeardown {
 
     @BeforeMethod
     protected void login() throws Exception {
+        if(browserName == null && browserVersion == null && platform == null && driverVersion == null) setEnvironment();
         loginPage = open("", LoginPage.class);
         loginPage.login(confVars.USER_LOGIN, confVars.USER_PASSWORD);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void createEnvironmentPropertiesFile() {
+        if(!new File("./build/allure-results/environment.properties").exists()) {
+            File file = new File("./build/allure-results/environment.properties");
+            List<String> lines = Arrays.asList(
+                    "Platform=" + platform,
+                    "Browser=" + browserName,
+                    "Browser.Version=" + browserVersion,
+                    "DriverVersion=" + driverVersion
+            );
+            Path filePath = Paths.get(file.getAbsolutePath());
+            try {
+                Files.write(filePath, lines, Charset.forName("UTF-8"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setEnvironment() {
+        Capabilities cap = ((RemoteWebDriver) getWebDriver()).getCapabilities();
+        browserName = cap.getBrowserName().substring(0, 1).toUpperCase() + cap.getBrowserName().substring(1).toLowerCase();
+        browserVersion = cap.getVersion();
+        platform = cap.getPlatform().toString();
+        if(browserName.toLowerCase().equals("chrome")) {
+            driverVersion = ChromeDriverManager.getInstance().getDownloadedVersion();
+        } else {
+            driverVersion = FirefoxDriverManager.getInstance().getDownloadedVersion();
+        }
     }
 }
